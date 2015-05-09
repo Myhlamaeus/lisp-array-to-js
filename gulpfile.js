e24b05b21b3cee17ca5fadb7b37494f32b8ed717
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-05-09 15:02:23 +0000

    Add missing gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp')
+var babel = require('gulp-babel')
+var sourcemaps = require('gulp-sourcemaps')
+var path = require('path')
+var main = path.basename(require('./package.json').main)
+require('babel/register')
+
+gulp.task('build', function () {
+  return gulp.src('{' + main + ',lib/**/*.js}')
+    .pipe(sourcemaps.init())
+      .pipe(babel())
+    .pipe(sourcemaps.write('../maps'))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('test', function () {
+  var mocha = require('gulp-mocha')
+
+  return gulp.src('test/*.js', {read: false})
+    .pipe(mocha())
+})
